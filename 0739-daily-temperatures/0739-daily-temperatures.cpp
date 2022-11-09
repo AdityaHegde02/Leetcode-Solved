53d3d3ca21dc4b5c9e727231f3485c2d06fad06b
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2022-11-09 17:03:05 +0530

    Time: 222 ms (83.09%), Space: 107.5 MB (6.40%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -6,16 +6,11 @@ public:
         stack<pair<int,int>> s;             // num,idx
         
         for(int i = 0; i < n; i++){
-            if(s.empty()){
-                s.push({t[i],i});
+            while(!s.empty() and s.top().first < t[i]){
+                ans[s.top().second] = i - s.top().second;
+                s.pop();
             }
-            else{
-                while(!s.empty() and s.top().first < t[i]){
-                    ans[s.top().second] = i - s.top().second;
-                    s.pop();
-                }
-                s.push({t[i],i});
-            }
+            s.push({t[i],i});
         }
         
         return ans;

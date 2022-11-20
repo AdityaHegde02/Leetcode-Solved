eba400e738fe6621ad21832b1d074327492204a5
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2022-11-20 11:10:42 +0530

    Time: 86 ms (5.71%), Space: 45 MB (5.24%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int smallestRepunitDivByK(int k) {
         vector<int> rems = {1};
-        for(int i = 0; i < 1000000; i++){
+        for(int i = 0; i < 50000; i++){
             rems.push_back((rems[i]*10 + 1) % k);
         }
-        for(int i = 0; i <= 1000000; i++){
+        for(int i = 0; i <= 50000; i++){
             if(rems[i] % k == 0){
                 return i + 1;
             }

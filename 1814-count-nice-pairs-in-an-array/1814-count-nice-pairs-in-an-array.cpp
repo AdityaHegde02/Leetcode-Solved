bbd06f204f2c3a6fd798b2baf50b3aebed4f01ab
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2022-11-30 17:26:20 +0530

    Time: 323 ms (39.29%), Space: 56.8 MB (90.67%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -3,8 +3,7 @@ public:
     int reverse(int num){
         int rev = 0;
         while(num != 0){
-            int r = num % 10;
-            rev = rev*10 + r;
+            rev = rev*10 + (num % 10);
             num /= 10;
         }
         return rev;

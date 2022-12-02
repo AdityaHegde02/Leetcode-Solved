50c54df02a7d67769060ae5be34d12fb00d19dcb
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2022-12-02 09:52:59 +0530

    Time: 4 ms (35.35%), Space: 6.1 MB (96.18%) - LeetHub

---
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumSwap(string s1, string s2) {
+        int xy = 0, yx = 0;
+        
+        for(int i = 0; i < s1.size(); i++){
+            if(s1[i] != s2[i] and s1[i] == 'x'){
+                xy++;
+            }
+            if(s1[i] != s2[i] and s1[i] == 'y'){
+                yx++;
+            }
+        }
+        
+        if((xy + yx) % 2){
+            return -1;
+        }
+        
+        return (xy/2) + (yx/2) + ((xy % 2) + (yx % 2));
+    }
+};

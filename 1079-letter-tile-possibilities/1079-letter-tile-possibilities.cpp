bd458d6b5bb0d70a6667a7c00bf02a9cf8fb2984
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2022-12-03 10:40:26 +0530

    Time: 1321 ms (5.05%), Space: 15.1 MB (43.67%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
-    void solve(int n,string &temp,string &tiles,vector<string> &ans,vector<int> &visited){
-        if(temp.size()){
-            ans.push_back(temp);
+    void solve(int n,string &temp,string &tiles,unordered_set<string> &s,vector<int> &visited){
+        if(temp.size() and s.find(temp) == s.end()){
+            s.insert(temp);
         }
         
         for(int j = 0; j < n; j++){
             if(!visited[j]){
                 visited[j] = 1;
                 temp += tiles[j];
-                solve(n,temp,tiles,ans,visited);
+                solve(n,temp,tiles,s,visited);
                 temp.pop_back();
-                solve(n,temp,tiles,ans,visited);
+                solve(n,temp,tiles,s,visited);
                 visited[j] = 0;
             }
         }
     }
     
     int numTilePossibilities(string tiles) {
-        vector<string> ans;
         int n = tiles.size();
+        unordered_set<string> s;
 
         for(int i = 0; i < n; i++){
             vector<int> visited(n,0);
             string temp;
             temp += tiles[i];
             visited[i] = 1;
-            solve(n,temp,tiles,ans,visited);
+            solve(n,temp,tiles,s,visited);
         }
-        
-        unordered_set<string> s;
-        
-        for(auto &i:ans){
-            s.insert(i);
-        }
-        
+    
         return s.size();
     }
 };

05d325c34adc5f8e34ac8cbcffad121505696355
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2022-12-19 12:16:15 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (53.44%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool wordPattern(string p, string s) {
+        int m = p.size();
+        unordered_map<char,string> mp;      // to map
+        stringstream ss(s);
+        string word;
+        vector<string> words;
+        unordered_map<string,bool> isMapped;
+        while(ss >> word){
+            words.push_back(word);
+        }
+        if(words.size() != m){
+            return false;
+        }
+        for(int i = 0; i < m; i++){
+            if(mp.find(p[i]) == mp.end()){
+                if(isMapped.find(words[i]) != isMapped.end()){
+                    return false;
+                }
+                mp[p[i]] = words[i];
+                isMapped[words[i]] = 1;
+            }
+            else{
+                if(words[i] != mp[p[i]]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};

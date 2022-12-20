c93546d4a36a78e3b4d49ac2359862afe9eb1ef5
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2022-12-20 09:11:05 +0530

    Time: 24 ms (28.67%), Space: 10.5 MB (35.11%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    unordered_set<int> keys;
-    
-    void solve(int i,int n,vector<vector<int>> &rooms){
-        if(i >= n or keys.size() == n){
-            return ;
-        }
-        
-        keys.insert(i);
-        
-        for(auto &k : rooms[i]){
-            if(keys.find(k) == keys.end()){
-                keys.insert(k);
-                solve(k,n,rooms);
-            }
-        }
-    }
-    
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
-        solve(0,n,rooms);
+        unordered_set<int> keys;
+        queue<int> q;
+        for(int &i : rooms[0]){
+            q.push(i);
+        }
+        keys.insert(0);
+        while(!q.empty()){
+            int n = q.size();
+            for(int i = 0; i < n; i++){
+                int currKey = q.front();
+                q.pop();
+                keys.insert(currKey);
+                for(int &j : rooms[currKey]){
+                    if(keys.count(j) == 0){
+                        q.push(j);
+                    }
+                }
+            }
+        }
         return (keys.size() == n);
     }
 };

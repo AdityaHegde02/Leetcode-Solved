89e5b83bbaaab6d5d5e21b2a7e733b5bca23fda0
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2023-01-25 14:26:25 +0530

    Added solution - LeetHub

---
+++ Type it! - GFG/type-it.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int minOperation(string s) {
+        int n = s.size();
+        int mx = 0;
+        string left;
+        for(int i = 0; i < n - 1; i++){
+            left += s[i];
+            string b = s.substr(i + 1,i + 1);
+            if(left == b){
+                mx = max(mx,i + 1);
+            }
+        }
+        return (mx == 0)? n : (n - mx + 1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.minOperation(s) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends

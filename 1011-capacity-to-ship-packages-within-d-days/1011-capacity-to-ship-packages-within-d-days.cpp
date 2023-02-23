644ae97532f23d3b0016f3fe95d1dcc6aa4e5783
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2023-02-23 10:50:36 +0530

    Time: 90 ms (16.63%), Space: 26.1 MB (37.43%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-    bool possiblecapacity(vector<int>& w,int cap,int d){
-        
-        int mxsum=cap,currsum=0;
-        for(int i=0;i<w.size();++i){
-            if(currsum+w[i]<=mxsum){
-                currsum+=w[i];
-                if(d==1 and i==w.size()-1){
-                    return true;
-                }
+    bool isPossible(int mid,vector<int> &w,int days){
+        int d = 1;
+        int curr = 0;
+        for(int i = 0; i < w.size(); i++){
+            if(w[i] > mid){
+                return false;
             }
-            else if(d>0){
-                currsum=0;
-                d--;
+            else if(curr + w[i] > mid){
+                d++;
+                curr = 0;
                 i--;
-                if(d==0 and i!=w.size()-1){
-                    return false;
-                }
+            }
+            else{
+                curr += w[i];
             }
         }
-        return true;
+        
+        if(d <= days){
+            return true;
+        }
+        return false;
     }
     
     int shipWithinDays(vector<int>& w, int d) {
-        int mx=INT_MIN,sum=0,ans=0;
-        for(int i=0;i<w.size();++i){
-            sum+=w[i];
-            mx=max(w[i],mx);
-        }
-        for(int i=mx;i<=sum;i++){
-            if(possiblecapacity(w,i,d)){
-                return i;
+        int sum = accumulate(w.begin(),w.end(),0);
+        int low = 0;
+        int high = sum;
+        int cap = INT_MAX;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(isPossible(mid,w,d)){
+                cap = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
             }
         }
-        return -1;
+        return cap;
     }
 };

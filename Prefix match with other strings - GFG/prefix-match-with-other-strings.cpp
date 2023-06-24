f902fdc41d2b7caf98158f2293bf9b022926bc1e
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2023-06-24 11:46:30 +0530

    Added solution - LeetHub

--- Prefix match with other strings - GFG/prefix-match-with-other-strings.cpp
+++ Prefix match with other strings - GFG/prefix-match-with-other-strings.cpp
@@ -9,19 +9,57 @@ using namespace std;
 
 class Solution{   
 public:
-    int klengthpref(string arr[], int n, int k, string str){    
-        int ans = 0;
+    struct TrieNode{
+        int passing_words;
+        TrieNode* children[26];
         
-        for(int i = 0; i < n; i++){
-            int sz = arr[i].size();
-            if(sz >= k){
-                if(str.substr(0,k) == arr[i].substr(0,k)){
-                    ans++;
-                }
+        TrieNode(){
+            passing_words = 0;
+            for(int i = 0; i < 26; i++){
+                children[i] = NULL;
             }
         }
+    };
+    
+    void insert(TrieNode* root,string &word){
+        TrieNode* node = root;
+        for(char &c : word){
+            int idx = c - 'a';
+            if(node->children[idx] == NULL){
+                node->children[idx] = new TrieNode();
+            }
         
-        return ans;
+            node = node->children[idx];
+            
+            node->passing_words++;
+        }
+       
+    }
+    
+    int search(TrieNode* root,string &word){
+        TrieNode* node = root;
+        for(char &c : word){
+            int idx = c - 'a';
+            if(node->children[idx] == NULL){
+                return 0;
+            }
+            node = node->children[idx];
+        }
+        return node->passing_words;
+    }
+    
+    int klengthpref(string arr[], int n, int k, string str){    
+        TrieNode* root = new TrieNode();
+        if(k > str.size()){
+            return  0;
+        }
+        for(int i = 0; i < n; i++){
+            insert(root,arr[i]);
+        }
+        
+        string s = str.substr(0,k);
+        
+        return search(root,s);
     }
 };
 

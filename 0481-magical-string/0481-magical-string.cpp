899d5317b2354302609d7a09ce122bd05fd344ae
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2023-07-23 18:26:43 +0530

    Time: 84 ms (5.09%), Space: 7.6 MB (90.68%) - LeetHub

---
+++ 0481-magical-string/0481-magical-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int magicalString(int n) {
+        string s = "1";
+        int curr = 2,times = 2,idx = 1;
+        while(s.size() <= n){
+            if(curr == 1){
+                for(int i = 0; i < times; i++){
+                    s += to_string(curr);
+                }
+                curr = 2;
+            }
+            else{
+                for(int i = 0; i < times; i++){
+                    s += to_string(curr);
+                }
+                curr = 1;
+            }
+            idx++;
+            times = s[idx] - '0';
+        }
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans += (s[i] == '1');
+        }
+        
+        return ans;
+    }
+};

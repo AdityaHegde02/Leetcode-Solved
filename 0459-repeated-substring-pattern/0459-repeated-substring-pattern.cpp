8170eea57d627ef05b65066bec20b289505589dc
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2023-08-21 17:39:28 +0530

    Time: 138 ms (19.91%), Space: 381.9 MB (12.46%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -6,9 +6,10 @@ public:
         for(int len = 1; len <= (m/2); len++){
             int i = 0;
             string start = s.substr(0,len);
-            if(m % len != 0){
+            if((m % len) != 0){
                 continue;
             }
+            i += len;
             while(i < m){
                 string curr = s.substr(i,len);
                 if(curr != start){

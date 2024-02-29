caec075c46c34a7628e736186df76ccecd698e55
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2024-02-29 20:31:17 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (31.19%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        sort(c.begin(),c.end());
+        int n = c.size();
+        int l = 0, r = n - 1, ans = 0;
+        while(l <= r){
+            int mid = (l + r)/2;
+            int papersPublished = n - mid, minCitationsGot = c[mid];
+            if(papersPublished >= minCitationsGot){
+                ans = max(ans,minCitationsGot);
+                l = mid + 1;
+            }
+            else{
+                ans = max(ans,papersPublished);
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};

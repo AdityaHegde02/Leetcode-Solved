e42add360726f9b36b689e46fed76259874b81ce
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2024-02-29 20:13:20 +0530

    Time: 13 ms (53.67%), Space: 20.9 MB (37.52%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     int hIndex(vector<int>& c) {
-        int n = c.size(), ans = 0;
-        for(int i = 0; i < n; i++){
-            ans = max(ans,min(n - i,c[i]));
+        int n = c.size();
+        int l = 0, r = n - 1, ans = 0;
+        while(l <= r){
+            int mid = (l + r)/2;
+            int papersPublished = n - mid, minCitationsGot = c[mid];
+            if(papersPublished >= minCitationsGot){
+                ans = max(ans,minCitationsGot);
+                l = mid + 1;
+            }
+            else{
+                ans = max(ans,papersPublished);
+                r = mid - 1;
+            }
         }
         return ans;
     }

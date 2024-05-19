21c4ca0a82ca3a00b89fbe624d304a495bcdd671
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2024-05-19 11:50:18 +0530

    Time: 2152 ms (20.00%), Space: 125.5 MB (100.00%) - LeetHub

---
+++ 3152-special-array-ii/3152-special-array-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<bool> ans;
+        vector<int> fail;
+        for(int i = 0; i < nums.size() - 1; i++){
+            if((nums[i] % 2) == (nums[i + 1] % 2)){
+                fail.push_back(i);
+            }
+        }
+        
+        for(int i = 0; i < queries.size(); i++){
+            int s = queries[i][0], e = queries[i][1] - 1;
+            for(auto &j : fail){
+                if(j >= s and j <= e){
+                    ans.push_back(0);
+                    break;
+                }
+            }
+            if(ans.size() != i + 1){
+                ans.push_back(1);
+            }
+        }
+        return ans;
+    }
+};

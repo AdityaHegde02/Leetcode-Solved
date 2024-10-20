209f6bc7c8ac9e4cde960a08b79795c26e03087f
Aditya Hegde <101964923+AdityaHegde02@users.noreply.github.com>
2024-10-20 17:44:28 +0530

    Time: 205 ms (26.17%), Space: 112.7 MB (5.02%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
-    int smallestChair(vector<vector<int>>& t, int tF) {
-        int currTime = 1;
-        
-        // map of at this time who arrives
-        unordered_map<int,int> arrive;
-        for(int i = 0; i < t.size(); i++){
-            arrive[t[i][0]] = i;
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        set<int> chairs;
+        int n = times.size();
+
+        for(int i = 0; i<n; i++)
+        {
+            chairs.insert(i);
         }
-        
-        //map of at this time who all departures
-        unordered_map<int,vector<int>> dep;
-        for(int i = 0; i < t.size(); i++){
-            dep[t[i][1]].push_back(i);
+
+        vector<vector<int>> t;
+
+        for(int i = 0; i<n; i++)
+        {
+            int arr = times[i][0];
+            int dep = times[i][1];
+
+            t.push_back({arr, 1, i});
+            t.push_back({dep, 0, i});
         }
-        
-        set<int> seats;
-        for(int i = 0; i < t.size(); i++){
-            seats.insert(i);
-        }
-        
-        // map of who all are in which seat
-        unordered_map<int,int> mapping;
-        
-        while(true){
-            // remove all empty chairs at that time
-            for(auto &i : dep[currTime]){
-                seats.insert(mapping[i]);
+
+        sort(t.begin(), t.end());
+
+        vector<int> ans(n, 0);
+
+        for(int i = 0; i<t.size(); i++)
+        {
+            if(t[i][1] == 1) // arrival
+            {
+                int ch = *chairs.begin();
+                ans[t[i][2]] = ch;
+                chairs.erase(ch);
             }
-            // add arrival to first empty seat
-            if(arrive.count(currTime)){
-                int s = *seats.begin();
-                // cout << s << endl;
-                if(arrive[currTime] == tF)
-                    return s;
-                mapping[arrive[currTime]] = s;
-                seats.erase(s);
+            else
+            {
+                int ch = ans[t[i][2]];
+                chairs.insert(ch);
             }
-            
-            currTime++;
         }
-        
-        return -1;
+
+        return ans[targetFriend];
     }
-};                       
+};                   
